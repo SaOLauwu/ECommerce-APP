04e0ed2f2b03e4d2569ec98a51e3b5095a6e1449
Santiago Álvarez <138623046+SaOLauwu@users.noreply.github.com>
2023-11-15 17:48:08 -0300

    Update Creacion DB.sql con consultas finales

--- Creacion DB.sql
+++ Creacion DB.sql
@@ -172,6 +172,7 @@ INSERT INTO Paquetes (Descripcion, Peso, Estado, Ci, ID_Lote, ID_Almacen) VALUES
 -- ... Resto de los paquetes
 ;
 
+/*
 -- CONSULTAS SQL
 -- 1 Paquetes entregados en Mayo 2023 con destino a Melo:
 SELECT P.ID_Paquete, P.Descripcion
@@ -249,3 +250,68 @@ SELECT A.ID_Almacen, A.Ubicacion
 FROM Almacenes A
 WHERE A.IDRuta = 2;
 
+-- CONSULTAS OPCIONALES
+-- Lista de los paquetes y su estado en un almacén específico:
+SELECT ID_Paquete, Estado
+FROM Paquetes
+WHERE ID_Almacen = 1; 
+-- Lotes que llegaron a un almacén específico durante agosto del 2023:
+SELECT ID_Lote, Fecha_Creacion
+FROM Lotes
+WHERE AlmacenDestino = [ID del Almacén] -- Reemplaza [ID del Almacén] con el ID específico
+AND MONTH(Fecha_Creacion) = 8
+AND YEAR(Fecha_Creacion) = 2023;
+-- Información de los camiones que actualmente se encuentran en ruta:
+SELECT T.Matricula, P.Descripcion AS Carga, R.Destino, R.Duracion_Estimada
+FROM Transportes T
+JOIN RutasAsignadas RA ON T.Matricula = RA.CiChofer
+JOIN Ruta R ON RA.ID_Ruta = R.ID_Ruta
+JOIN Paquetes P ON RA.ID_Asignacion = P.ID_Almacen -- Asumiendo que se relaciona así
+WHERE RA.Estado = 'En curso';
+-- Información de un paquete específico que haya sido entregado:
+SELECT P.ID_Lote, RA.ID_Ruta, T.Matricula, A.ID_Almacen, T2.Matricula AS CamionetaUltimoTramo, C.Direccion
+FROM Paquetes P
+JOIN Lotes L ON P.ID_Lote = L.ID_Lote
+JOIN RutasAsignadas RA ON L.ID_Lote = RA.ID_Asignacion -- Asumiendo que se relaciona así
+JOIN Transportes T ON RA.CiChofer = T.Matricula
+JOIN Almacenes A ON L.AlmacenDestino = A.ID_Almacen
+JOIN Clientes C ON P.Ci = C.Ci
+JOIN Transportes T2 ON A.ID_Almacen = T2.Matricula -- Asumiendo que se relaciona así
+WHERE P.ID_Paquete = [ID del Paquete] AND P.Estado = 'Entregado'; -- Reemplaza [ID del Paquete] con el ID específico
+-- Recorridos realizados y almacenes visitados por un camión en el último mes:
+SELECT RA.ID_Ruta, A.ID_Almacen
+FROM RutasAsignadas RA
+JOIN Almacenes A ON RA.ID_Ruta = A.IDRuta
+WHERE RA.CiChofer = [Matricula del Camión] -- Reemplaza [Matricula del Camión] con la matrícula específica
+AND RA.Fecha BETWEEN DATE_SUB(CURDATE(), INTERVAL 1 MONTH) AND CURDATE();
+-- Paquetes entregados en julio de 2023, ordenados por fecha de entrega descendente:
+SELECT P.ID_Paquete, E.Fecha_Hora AS FechaEntrega
+FROM Paquetes P
+JOIN Eventos E ON P.ID_Paquete = E.ID_Paquete
+WHERE E.Tipo_Evento = 'Entregado'
+AND MONTH(E.Fecha_Hora) = 7
+AND YEAR(E.Fecha_Hora) = 2023
+ORDER BY E.Fecha_Hora DESC;
+-- Camiones que no hicieron ningún recorrido entre el 10 y 17 de julio de 2023:
+SELECT T.Matricula
+FROM Transportes T
+LEFT JOIN RutasAsignadas RA ON T.Matricula = RA.CiChofer
+WHERE (RA.Fecha IS NULL OR RA.Fecha NOT BETWEEN '2023-07-10' AND '2023-07-17')
+AND T.Matricula NOT IN (
+  SELECT RA.CiChofer
+  FROM RutasAsignadas RA
+  WHERE RA.Fecha BETWEEN '2023-07-10' AND '2023-07-17'
+);
+-- Lista de almacenes que pertenecen a un recorrido dado y su distancia con el centro de distribución:
+SELECT A.ID_Almacen, A.Ubicacion, R.Distancia
+FROM Almacenes A
+JOIN Ruta R ON A.IDRuta = R.ID_Ruta
+WHERE R.ID_Ruta = [ID de la Ruta]; -- Reemplaza [ID de la Ruta] con el ID específico
+-- Recorridos utilizados en mayo del 2023, ordenados por la cantidad de camiones:
+SELECT RA.ID_Ruta, COUNT(DISTINCT RA.CiChofer) AS CantidadCamiones
+FROM RutasAsignadas RA
+WHERE MONTH(RA.Fecha) = 5 AND YEAR(RA.Fecha) = 2023
+GROUP BY RA.ID_Ruta
+ORDER BY CantidadCamiones DESC;
+
+*/
